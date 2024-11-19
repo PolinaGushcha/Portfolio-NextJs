a2eed5f6390c181b935c6bd2af01f5ee56b0dffd
Polinasss <polina129889@gmail.com>
2024-11-19 19:49:48 +0100

    add: generate next project with prettier and config & consts files

---
+++ app/config/pages-url.config.ts
@@ -0,0 +1,11 @@
+class DASHBOARD {
+  private root = '/i'
+
+  HOME = this.root
+  EXPERIENCE = `${this.root}/experience`
+  SKILLS = `${this.root}/skills`
+  WORKS = `${this.root}/works`
+  CONTACTS = `${this.root}/contacts`
+}
+
+export const DASHBOARD_PAGES = new DASHBOARD()
